03cb596440059d00f7d03321a75518a5091b39d8
AgAndres98 <aandres@invertironline.com>
2022-05-26 15:13:16 -0300

    se agrega boton de solicitud de objeto funcionando

---
+++ src/components/Objeto/BtnRequest/BtnRequest.styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  content: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  btnContainer: {
+    marginTop: 20,
+    width: "90%",
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  btnReq: {
+    backgroundColor: "#62bd60",
+  },
+  btnCancel: {
+    backgroundColor: "#F02B2F",
+  },
+});
