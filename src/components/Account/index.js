10e32cb177455ac9c0cc3feeebc8f1becca5d8b9
AgustinFaginas <agustin.faginas@hotmail.com>
2022-05-27 22:48:33 -0300

    change account index

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -2,11 +2,8 @@ export * from "./InfoUser";
 export * from "./AccountOptions";
 export * from "./ChangeEmailForm";
 export * from "./ChangePasswordForm";
-<<<<<<< HEAD
 export * from "./DonorQuestionnaireForm"
 export * from "./BeneficiaryQuestionnaireForm"
 
-=======
 export * from "./DonorQuestionnaireForm";
 export * from "./MyObjects";
->>>>>>> agustinF
