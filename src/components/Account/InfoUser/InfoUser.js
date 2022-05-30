321450ff5ec2525cea822a82ee0068f51d2988c2
AgAndres98 <aandres@invertironline.com>
2022-05-29 23:36:42 -0300

    fix bug de nombre en cuenta

--- src/components/Account/InfoUser/InfoUser.js
+++ src/components/Account/InfoUser/InfoUser.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import { DatePickerIOSBase, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { DatePickerIOSBase, View } from "react-native";
 import { Avatar, Text } from "react-native-elements";
 import * as ImagePicker from "expo-image-picker";
-import { getAuth, updateProfile } from 'firebase/auth';
+import { getAuth, updateProfile } from "firebase/auth";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { styles } from "./InfoUser.styles";
 import {
@@ -15,9 +15,6 @@ import {
   Firestore,
 } from "firebase/firestore";
 import { db } from "../../../utils";
-
-
-
 
 export function InfoUser(props) {
   const { setLoading, setLoadingText } = props;
@@ -34,7 +31,6 @@ export function InfoUser(props) {
     );
 
     onSnapshot(q, async (snapshots) => {
-
       for await (const item of snapshots.docs) {
         const data = item.data();
         const docRef = doc(db, "datosPersonales", data.id);
@@ -42,10 +38,9 @@ export function InfoUser(props) {
         const newData = docSnap.data();
         newData.id = data.id;
         setDatos(newData);
-      };
+      }
     });
   }, []);
-
 
   console.log("DATOS INFOUSER");
   console.log(datos);
@@ -71,7 +66,6 @@ export function InfoUser(props) {
 
     uploadBytes(storageRef, blob).then((snapshot) => {
       updatePhotoUrl(snapshot.metadata.fullPath);
-
     });
   };
 
@@ -88,7 +82,8 @@ export function InfoUser(props) {
 
   return (
     <View style={styles.content}>
-      <Avatar size="large"
+      <Avatar
+        size="large"
         rounded
         containerStyle={styles.avatar}
         icon={{ type: "material", name: "person" }}
@@ -97,7 +92,11 @@ export function InfoUser(props) {
         <Avatar.Accessory size={24} onPress={changeAvatar} />
       </Avatar>
       <View>
-        <Text style={styles.displayName}>{datos.nombre} {datos.apellido}</Text>
+        {datos !== null && (
+          <Text style={styles.displayName}>
+            {datos.nombre} {datos.apellido}
+          </Text>
+        )}
         <Text>{email}</Text>
       </View>
     </View>
@@ -106,6 +105,6 @@ export function InfoUser(props) {
 
 function avatarUri(avatar) {
   if (avatar !== null) {
-    return uri = { uri: avatar };
+    return (uri = { uri: avatar });
   }
 }
