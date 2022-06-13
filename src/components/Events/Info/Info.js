83de741daf176782be2b79283c095ee8ff633b11
AgustinFaginas <agustin.faginas@hotmail.com>
2022-06-13 15:37:38 -0300

    mejoras a detalle evento

--- src/components/Events/Info/Info.js
+++ src/components/Events/Info/Info.js
@@ -1,22 +1,47 @@
 import React from "react";
 import { View } from "react-native";
-import { Text } from "react-native-elements";
+import { Text, Icon, ListItem } from "react-native-elements";
 import { styles } from "./Info.styles";
 import { Map } from "../../Shared";
+import { map } from "lodash";
+import { List } from "react-native-paper";
 
 export function Info(props) {
   const { evento } = props;
 
+  const listInfo = [{
+    text: evento.telefono,
+    iconType: "material-community",
+    iconName: "phone",
+  },
+  {
+    text: evento.email,
+    iconType: "material-community",
+    iconName: "at",
+  },
+  {
+    text: evento.direccion,
+    iconType: "material-community",
+    iconName: "map-marker",
+  },
+  ];
 
   return (
     <View style={styles.content}>
       <Text style={styles.title}>Información</Text>
       <Text style={styles.fecha}>{evento.fecha}</Text>
-      <Text style={styles.texto}>Organizador :  <Text style={styles.description}>{evento.organizador}</Text></Text>
-      <Text style={styles.texto}>Ubicación :  <Text style={styles.description} > {evento.direccion} </Text>  </Text>
-      <Map ubicacion={evento.ubicacion} titulo={evento.titulo} />
-      <Text style={styles.texto}> Email de contacto :  <Text style={styles.description}>{evento.email}</Text></Text>
-      <Text style={styles.texto}> Teléfono :  <Text style={styles.description}>{evento.telefono}</Text></Text>
+      {map(listInfo, (item, index) => (
+        <ListItem style={styles.listInfo} key={index} bottomDivider>
+          <Icon
+            type={item.iconType}
+            name={item.iconName}
+            color="#00a680" />
+          <ListItem.Content>
+            <ListItem.Title>{item.text}</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>))}
+
+      <Map style={styles.mapa} ubicacion={evento.ubicacion} titulo={evento.titulo} />
     </View>
   );
 }
