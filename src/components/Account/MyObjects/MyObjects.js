ba44f27b2ff86591f6d44843e95cd4deb2fa174c
AgAndres98 <aandres@invertironline.com>
2022-06-16 16:42:16 -0300

    se hace bien el borrado de todos los componentes

--- src/components/Account/MyObjects/MyObjects.js
+++ src/components/Account/MyObjects/MyObjects.js
@@ -10,12 +10,11 @@ import {
   collection,
   query,
   where,
-  onSnapshot,
-  deleteDoc
+  getDocs,
+  deleteDoc,
 } from "firebase/firestore";
 import { forEach } from "lodash";
 import { async } from "@firebase/util";
-
 
 export function MyObjects(props) {
   const { objects } = props;
@@ -27,7 +26,6 @@ export function MyObjects(props) {
 
   const toggleSwitch = (idObjeto, activa) => {
     try {
-      console.log(activa);
       if (activa == true) {
         setIsEnabled(false);
       }
@@ -37,72 +35,58 @@ export function MyObjects(props) {
       updateDoc(doc(db, "objetos", idObjeto), {
         activa: isEnabled ? true : false,
       });
-
     } catch (error) {
       console.log("error");
     }
   };
 
   const goToRequest = (idObjeto, tipoObjeto) => {
-
-    console.log(idObjeto);
-    navigation.navigate(screen.account.userRequests, { idObjeto: idObjeto, tipoObjeto: tipoObjeto });
-  }
-
-
-
-
-
-
-  const onRemoveObject = async (id) => {
-
-    console.log(id);
-
-    try {
-
-      const q = query(
-        collection(db, "favorites"),
-        where("idObjeto", "==", id)
-      );
-
-      const length = favorites.length;
-      console.log(length);
-
-      onSnapshot(q, (snapshot) => {
-        setFavorites(snapshot.docs);
-        forEach(favorites, async (item) => {
-          console.log("BORRAR FAVORITO");
-          console.log(item.id);
-          await deleteDoc(doc(db, "favorites", item.id));
-          favorites.splice(favorites[item])
-        });
-      });
-
-
-      const q2 = query(
-        collection(db, "requests"),
-        where("idObjeto", "==", id)
-      );
-      onSnapshot(q2, (snapshot) => {
-        setRequest(snapshot.docs);
-
-
-        forEach(request, async (item) => {
-          console.log("BORRAR REQUEST");
-          console.log(item.id);
-          await deleteDoc(doc(db, "requests", item.id));
-        });
-      });
-
-
-      await deleteDoc(doc(db, "objetos", id));
-
-    } catch (error) {
-      console.log("error");
-    }
+    navigation.navigate(screen.account.userRequests, {
+      idObjeto: idObjeto,
+      tipoObjeto: tipoObjeto,
+    });
   };
 
+  const getFavorites = async (idObjeto) => {
+    const q = query(
+      collection(db, "favorites"),
+      where("idObjeto", "==", idObjeto)
+    );
 
+    const result = await getDocs(q);
+    return result.docs;
+  };
+
+  const getRequested = async (idObjeto) => {
+    const q = query(
+      collection(db, "requests"),
+      where("idObjeto", "==", idObjeto)
+    );
+
+    const result = await getDocs(q);
+    return result.docs;
+  };
+
+  const onRemoveObject = async (id) => {
+    console.log(id);
+    try {
+      const favoritesCollection = await getFavorites(id);
+
+      forEach(favoritesCollection, async (item) => {
+        await deleteDoc(doc(db, "favorites", item.id));
+      });
+
+      const requestsCollection = await getRequested(id);
+
+      forEach(requestsCollection, async (item) => {
+        await deleteDoc(doc(db, "requests", item.id));
+      });
+
+      await deleteDoc(doc(db, "objetos", id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <View style={styles.screen}>
@@ -126,7 +110,9 @@ export function MyObjects(props) {
                       trackColor={{ false: "#767577", true: "#767577" }}
                       thumbColor={objeto.activa ? "#62bd60" : "#f4f3f4"}
                       ios_backgroundColor="#62bd60"
-                      onValueChange={() => { toggleSwitch(objeto.id, objeto.activa) }}
+                      onValueChange={() => {
+                        toggleSwitch(objeto.id, objeto.activa);
+                      }}
                       value={objeto.activa}
                       style={styles.switch}
                     />
@@ -142,7 +128,7 @@ export function MyObjects(props) {
                       name="pencil-outline"
                       size={35}
                       containerStyle={styles.edit}
-                    // onPress={console.log("editar1")}
+                      // onPress={console.log("editar1")}
                     />
 
                     <Icon
@@ -150,7 +136,9 @@ export function MyObjects(props) {
                       name="delete-outline"
                       size={35}
                       containerStyle={styles.delete}
-                      onPress={() => { onRemoveObject(objeto.id) }}
+                      onPress={() => {
+                        onRemoveObject(objeto.id);
+                      }}
                     />
                   </View>
                 </View>
@@ -161,7 +149,9 @@ export function MyObjects(props) {
                   title={"Ver solicitudes"}
                   containerStyle={styles.btnContainer}
                   buttonStyle={styles.btnSolicitudes}
-                  onPress={() => { goToRequest(objeto.id, objeto.tipo) }}
+                  onPress={() => {
+                    goToRequest(objeto.id, objeto.tipo);
+                  }}
                 />
               </View>
             </View>
@@ -171,7 +161,3 @@ export function MyObjects(props) {
     </View>
   );
 }
-
-
-
-
